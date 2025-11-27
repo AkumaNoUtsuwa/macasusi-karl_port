197fcd0d1d9073fb5b7a781b6bd63c5a9ae48bb5
AkumaNoUtsuwa <karlmacasusi717@gmail.com>
2025-11-27 11:38:15 +0800

    commit

--- my-nextjs-app/Components/Sections/Work_Sec.tsx
+++
@@ -1,7 +0,0 @@
-export default function WorkSection(){
-    return(
-        <div className='bg-blue-300 h-screen text-2xl text-center content-center'>
-            This are my Works
-        </div>
-    )
-}
